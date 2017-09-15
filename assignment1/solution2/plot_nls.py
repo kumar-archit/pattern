eebc768b613ea0c117146e561bc6b188a0b1643d
avi224 <avinav.65536@gmail.com>
2017-09-16 00:00:22 +0530

    v4

--- assignment1/solution2/plot_nls.py
+++ assignment1/solution2/plot_nls.py
@@ -10,12 +10,16 @@ avg=[]
 cov0=[[0,0],[0,0]]
 cov=[]
 cinv=[]
+test=[]
 x=[]
 W=[]
 w=[]
 n=[0,0,0]
 col=["red","blue","green"]
 k=0
+tot=0
+def fn(X,k):
+    return -0.5*math.log(np.linalg.det(cinv[k]))+math.log(n[k]*1.0/tot)-0.5*np.dot(np.dot(np.transpose(np.subtract(X,avg[k])),cinv[k]),np.subtract(X,avg[k]))
 def g(i,j,cov1,cov2):
     ci=np.linalg.inv(cov1)
     cj=np.linalg.inv(cov2)
@@ -60,6 +64,7 @@ for file in files:
         x[i][0].append(float(c))
         x[i][1].append(float(d))
         n[i]+=1
+    tot+=n[i]
     cov[i][0][1]=cov[i][1][0]=co(x[i][0],x[i][1],n[i])
     cov[i][0][0]=co(x[i][0],x[i][0],n[i])
     cov[i][1][1]=co(x[i][1],x[i][1],n[i])
@@ -77,4 +82,41 @@ for j in range(0,2):
 for i in range(0,2):
     #g(i,(i+1)%2,cov[i],cov[(i+1)%2])
     g(i,(i+1)%2,cov0,cov0)
+path = '/media/avi224/Local Disk/Sem5/CS669/pattern/assignment1/solution2/Test/*.txt'
+i=0
+for file in files: 
+    f=open(file,'r')
+    test.append([])
+    for line in f:
+        c,d=line.split()
+        test[i].append([float(c),float(d)])
+    i+=1
+cnt=[]
+for i in range(0,2):
+    cnt.append([0,0,0])
+to=0
+for i in range(0,2):
+    correct=0
+    for j in test[i]:
+        to+=1
+        max=0
+        maxi=-100000000000
+        for k in range(0,2):
+            if fn(j,k)>maxi:
+                max=k
+                maxi=fn(j,k)
+        for k in range(0,2):
+            if fn(j,k)==maxi:
+                cnt[i][k]+=1
+    correct+=cnt[i][i]
+    print(cnt[i][0], cnt[i][1], cnt[i][2])
+print("classification accuracy:",correct/to)
+for i in range(0,2):
+    to=0
+    to1=0
+    for j in range(0,2):
+        to+=cnt[j][i]
+        to1+=cnt[i][j]
+    print("precision of class",i+1,":",cnt[i][i]/to)
+    print("recall of class",i+1,":",cnt[i][i]/to1)
 pl.show()
